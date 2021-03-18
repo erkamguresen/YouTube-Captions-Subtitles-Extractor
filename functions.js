96a4a18120c82f4228a583807ee547a1853ff883
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-03-18 07:36:34 +0100

    get subtitle function is added

--- functions.js
+++ functions.js
@@ -115,3 +115,32 @@ console.log(createdPerson2("Zodiac Hasbro", 56, "male"));
 
 document.getElementById('target').appendChild(  document.getElementById('to_be_moved') );
 */
+
+// function(){
+//     var regexp %3D new RegExp(
+//         %2FplayerCaptionsTracklistRenderer.*%3F(
+//             youtube.com%5C%2Fapi%5C%2Ftimedtext.*%3F
+//             )
+//     "%2F
+//     )
+//     %3B%0Avar match %3D regexp.exec
+//     (document.body.innerHTML)%3B%0Aif (!match) %7B%0A
+//     alert ("No captions found")%3B%0A
+//     return%3B%0A%7D%0Avar url %3D regexp.exec(document.body.innerHTML)
+//     %5B1%5D%3B%0Aopen("http%3A%2F%2Ftechleadpro.com%2Fytcaption%2Fcaption.php%3Furl%3D" %2B encodeURIComponent(url))%3B }
+
+function getSubTitle() {
+  var regexp = new RegExp(
+    /playerCaptionsTracklistRenderer.*?(youtube.com\/api\/timedtext.*?)"/
+  );
+  var match = regexp.exec(document.body.innerHTML);
+  if (!match) {
+    alert("No captions found");
+    return;
+  }
+  var url = regexp.exec(document.body.innerHTML)[1];
+  open(
+    "http://techleadpro.com/ytcaption/caption.php?url=" +
+      encodeURIComponent(url)
+  );
+}
