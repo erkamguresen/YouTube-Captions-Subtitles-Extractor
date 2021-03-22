473f0f3aa1bf3a87aab63f264417f1153b3abf53
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-03-22 09:00:16 +0100

    re-naming variables

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function dividedWindow() {
-  var asideDiv = document.getElementsByClassName("asideDiv");
+  let asideDiv = document.getElementsByClassName("asideDiv");
 
   // check if the div exists
   if (asideDiv.length === 0) {
@@ -8,38 +8,102 @@ function dividedWindow() {
     asideDiv.className = "asideDiv";
 
     //create 2 divs' within the aside div
-    var newDiv = document.createElement("div");
+    let newDiv = document.createElement("div");
     newDiv.className = "asideDiv upperAsideDiv";
 
-    var tag = document.createElement("h1");
-    var text = document.createTextNode("this is the first part");
+    let tag = document.createElement("h1");
+    let text = document.createTextNode("this is the first part");
     tag.appendChild(text);
     newDiv.appendChild(tag);
     newDiv.setAttribute("id", "asideUpper");
 
     asideDiv.appendChild(newDiv);
 
-    newDiv = document.createElement("div");
-    newDiv.className = "asideDiv lowerAsideDiv";
+    let newDiv2 = document.createElement("div");
+    newDiv2.className = "asideDiv lowerAsideDiv";
 
     tag = document.createElement("h1");
     text = document.createTextNode("this is the second part");
     tag.appendChild(text);
-    newDiv.appendChild(tag);
+    newDiv2.appendChild(tag);
 
-    asideDiv.appendChild(newDiv);
+    asideDiv.appendChild(newDiv2);
 
     //append to the body element
     document.body.appendChild(asideDiv);
 
     //style with borders
     asideDiv.style.cssText = "height:100%;" + "border: 2px solid red";
-    newDiv.style.cssText = "height:50%; border:2px solid green";
+
+    newDiv2.style.cssText = "height:50%; border:2px solid green";
+
     document.getElementById("asideUpper").style.cssText =
       "height:50%;" + "border:2px solid blue";
 
     // make grid 2fr 1 fr
-    var body = document.body;
+    let body = document.body;
+    body.style.cssText =
+      "width:100%;" +
+      "height:100vh;" +
+      "border: 2px solid yellowgreen;" +
+      "padding:10px;" +
+      "display:grid; " +
+      "grid-template-columns:2fr 1fr";
+
+    //alert
+    alert("3 window will be initiated");
+  } else {
+    //alert
+    alert("3 window is already exists");
+  }
+}
+
+function dividedWindowWithIFrame() {
+  let asideDiv = document.getElementsByClassName("asideDiv");
+
+  // check if the div exists
+  if (asideDiv.length === 0) {
+    //create main panel and put an Iframe in it
+
+    //create aside div
+    asideDiv = document.createElement("div");
+    asideDiv.className = "asideDiv";
+
+    //create 2 divs' within the aside div
+    let upperDiv = document.createElement("div");
+    upperDiv.className = "asideDiv upperAsideDiv";
+
+    let tag = document.createElement("h1");
+    let text = document.createTextNode("this is the first part");
+    tag.appendChild(text);
+    upperDiv.appendChild(tag);
+    upperDiv.setAttribute("id", "asideUpper");
+
+    asideDiv.appendChild(upperDiv);
+
+    let lowerDiv = document.createElement("div");
+    lowerDiv.className = "asideDiv lowerAsideDiv";
+
+    tag = document.createElement("h1");
+    text = document.createTextNode("this is the second part");
+    tag.appendChild(text);
+    lowerDiv.appendChild(tag);
+
+    asideDiv.appendChild(lowerDiv);
+
+    //append to the body element
+    document.body.appendChild(asideDiv);
+
+    //style with borders
+    asideDiv.style.cssText = "height:100%;" + "border: 2px solid red";
+
+    lowerDiv.style.cssText = "height:50%; border:2px solid green";
+
+    document.getElementById("asideUpper").style.cssText =
+      "height:50%;" + "border:2px solid blue";
+
+    // make grid 2fr 1 fr
+    let body = document.body;
     body.style.cssText =
       "width:100%;" +
       "height:100vh;" +
