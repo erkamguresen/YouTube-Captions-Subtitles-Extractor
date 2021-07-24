741dd80cc9119d5fcdb2aedd6d6c513f568a833c
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-24 10:41:54 +0200

    controls are updated

--- youtube speed/speed.js
+++ youtube speed/speed.js
@@ -1,16 +1,50 @@
-javascript: (function () {
-  const v = document.querySelector('video');
-  v.playbackRate = v.playbackRate != 1 ? 1 : 5;
+function skipAds() {
+  const video = document.querySelector('video');
 
-  setTimeout(() => {
-    warnings.innerText = '';
-  }, 1000);
   const skipAdsButton = document.querySelector('.ytp-ad-skip-button');
-  let isSkipped = false;
-  skipAdsButton.click();
-})();
+  const smallAdsButton = document.querySelector('.ytp-ad-overlay-close-button');
 
-javascript: (function () {
-  var v = document.querySelector('video');
-  v.playbackRate = v.playbackRate != 1 ? 1 : 3;
+  if (skipAdsButton) {
+    video.playbackRate = video.playbackRate != 1 ? 1 : 5;
+
+    setTimeout(() => {
+      warnings.innerText = '';
+    }, 1000);
+
+    skipAdsButton.click();
+  }
+
+  if (smallAdsButton) {
+    smallAdsButton.click();
+  }
+}
+
+(function () {
+  skipAds();
+
+  document.onkeyup = function (event) {
+    const video = document.querySelector('video');
+    let e = event || window.event;
+
+    // ctrl + Enter to skip adds or reset video playbackRate
+    if (e.key === 'Enter' && e.ctrlKey) {
+      skipAds();
+      video.playbackRate = 1;
+    }
+
+    // + to decrease the video playback speed
+    if (e.key === '+') {
+      video.playbackRate += 0.25;
+    }
+
+    // - to decrease the video playback speed
+    if (e.key === '-' && video.playbackRate > 0.25) {
+      video.playbackRate -= 0.25;
+    }
+
+    // Alt + Ctrl to reset the video speed
+    if (e.key === '0' && e.ctrlKey) {
+      video.playbackRate = 1;
+    }
+  };
 })();
